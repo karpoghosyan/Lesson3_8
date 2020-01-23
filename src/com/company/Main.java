1a20711eba5a46e625652748f54eb4fb4e5bd3ce
karpoghosyan <karpoghosyan99@gmail.com>
2020-01-24 02:31:32 +0400

    .1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
